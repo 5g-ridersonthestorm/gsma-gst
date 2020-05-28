dc011d56421049fe93ccc952e0d5c4607f69673c
Paul Jordan <paul.m.jordan@bt.com>
2020-05-28 21:48:00 +0100

    add jinja2

---
+++ tosca/create_enablement_tosca.py
@@ -0,0 +1,27 @@
+import jinja2
+import sys
+import os
+
+
+
+template_filename = "mec_hosting_enablement.j2"
+rendered_filename = "mec_hosting_enablement.yml"
+# get arguments and set them in 
+# TODO get args using getopt and put them into render_vars instead of the hard code example below 
+render_vars = {
+    "app_image_file": "string from arg for image_file" 
+    "app_state_data": "string from arg for state" 
+    "app_public_url": "string from arg for url" 
+}
+
+script_path = os.path.dirname(os.path.abspath(__file__))
+template_file_path = os.path.join(current_path, template_filename)
+rendered_file_path = os.path.join(current_path, rendered_filename)
+
+# set render env
+render_environment = jinja2.Environment(loader=jinja2.FileSystemLoader(template_file_path))
+# render arguments into the jinja2 template
+output_text = environment.get_template(template_filename).render(render_vars)
+# write the result 
+with open(rendered_file_path, "w") as result_file:
+    result_file.write(output_text)
