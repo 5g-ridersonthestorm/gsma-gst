d1f966578a0131e4b4ee3b542bf982c7afb20684
Paul Jordan <paul.m.jordan@bt.com>
2020-06-02 14:21:00 +0100

    jinja works

--- tosca/create_enablement_tosca.py
+++ tosca/create_enablement_tosca.py
@@ -1,29 +1,26 @@
 import jinja2
 import sys
 import os
+import json
 
+# command line for use: python create_enablement_tosca.py '{"app_image_file": "http://cloudrepo:80/files/wheresmychargepointapp.iso","app_state_data": "data_url.com","app_public_url": "wheresmychargepointapp.com"}'
+# command line for use: python create_enablement_tosca.py '{"app_image_file": "http://cloudrepo:80/files/test.iso","app_state_data": "data_url.com","app_public_url": "test.com"}'
 
-
-
-# get arguments and set them in 
-# TODO get args using getopt and put them into render_vars instead of the hard code example below 
-render_vars = {
-    "app_image_file": "string from arg for image_file" 
-    "app_state_data": "string from arg for state" 
-    "app_public_url": "string from arg for url" 
-}
+# get arguments and set to render
+render_vars = json.loads(sys.argv[1])
 
 template_filename = "mec_hosting_enablement.j2"
-rendered_filename = "mec_hosting_enablement"+app_public_url+"".yml"
+rendered_filename = "mec_hosting_enablement"+render_vars.get("app_public_url")+".yml"
 
 script_path = os.path.dirname(os.path.abspath(__file__))
-template_file_path = os.path.join(current_path, template_filename)
-rendered_file_path = os.path.join(current_path, rendered_filename)
+template_file_path = os.path.join(os.curdir, template_filename)
+rendered_file_path = os.path.join(script_path, rendered_filename)
+
 
 # set render env
-render_environment = jinja2.Environment(loader=jinja2.FileSystemLoader(template_file_path))
+render_environment = jinja2.Environment(loader=jinja2.FileSystemLoader('templates'))
 # render arguments into the jinja2 template
-output_text = environment.get_template(template_filename).render(render_vars)
+output_text = render_environment.get_template(template_filename).render(render_vars)
 # write the result 
 with open(rendered_file_path, "w") as result_file:
     result_file.write(output_text)
